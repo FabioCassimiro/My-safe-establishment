ab9e5df9a0847bf6ed40ea860b4dc74afe0bb5e4
FabioCassimiro <fabiocassimiro.project@gmail.com>
2021-04-24 15:23:03 -0300

    Commit inicial.

---
+++ src/main/java/br/com/mysafeestablishment/MySafeEstablishmentApplication.java
@@ -0,0 +1,13 @@
+package br.com.mysafeestablishment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MySafeEstablishmentApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MySafeEstablishmentApplication.class, args);
+    }
+
+}
---
+++ src/test/java/br/com/mysafeestablishment/MySafeEstablishmentApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.mysafeestablishment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MySafeEstablishmentApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
