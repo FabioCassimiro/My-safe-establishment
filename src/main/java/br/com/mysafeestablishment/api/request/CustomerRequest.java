8a444eb635b100d05e56025c5626d8844e3bb68c
Fabio Cassimiro <fabiocassimiro.project@gmail.com>
2021-08-22 17:46:30 -0300

    MSE-20210821 - Adequação de login

---
+++ src/main/java/br/com/mysafeestablishment/api/request/CustomerRequest.java
@@ -0,0 +1,56 @@
+package br.com.mysafeestablishment.api.request;
+
+public class CustomerRequest {
+
+    private String name;
+    private String phoneNumber;
+    private String cpf;
+
+    public CustomerRequest(String name, String phoneNumber, String cpf) {
+        this.name = name;
+        this.phoneNumber = phoneNumber;
+        this.cpf = cpf;
+    }
+
+    public CustomerRequest(String phoneNumber, String cpf) {
+        this.phoneNumber = phoneNumber;
+        this.cpf = cpf;
+    }
+
+    public CustomerRequest() {
+
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    @Override
+    public String toString() {
+        return "CustomerRequest{" +
+                "name='" + name + '\'' +
+                ", phoneNumber='" + phoneNumber + '\'' +
+                ", cpf='" + cpf + '\'' +
+                '}';
+    }
+}
--- src/main/java/br/com/mysafeestablishment/service/user/CustomerService.java
+++ src/main/java/br/com/mysafeestablishment/service/user/CustomerService.java
@@ -1,11 +1,9 @@
 package br.com.mysafeestablishment.service.user;
 
+import br.com.mysafeestablishment.api.client.CustomerClient;
+import br.com.mysafeestablishment.api.request.CustomerRequest;
 import br.com.mysafeestablishment.domain.user.Customer;
 import br.com.mysafeestablishment.exception.RegisteredUserException;
-import br.com.mysafeestablishment.repository.user.CustomerRepository;
-import br.com.mysafeestablishment.service.OrderPadService;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -13,41 +11,21 @@ import org.springframework.stereotype.Service;
 
 @Service
 public class CustomerService {
-    private final Logger logger = LoggerFactory.getLogger(CustomerService.class);
 
-    CustomerRepository customerRepository;
-    OrderPadService orderPadService;
+    CustomerClient customerClient;
 
     @Autowired
-    public CustomerService(CustomerRepository customerRepository,OrderPadService orderPadService){
-        this.customerRepository = customerRepository;
-        this.orderPadService = orderPadService;
+    public CustomerService(CustomerClient customerClient) {
+        this.customerClient = customerClient;
     }
 
-    public ResponseEntity<String> register(Customer newCustomer){
-        try {
-            hasRegister(newCustomer);
-            customerRepository.save(newCustomer);
-        } catch (Exception e){
-            return new ResponseEntity<String>(e.getMessage(), HttpStatus.BAD_REQUEST);
-        }
-        return new ResponseEntity<String>("Custumer criado com sucesso", HttpStatus.CREATED);
+    public ResponseEntity<String> register(CustomerRequest newCustomer){
+        return customerClient.customerLogin(newCustomer);
     }
 
-    public ResponseEntity<String> login(Customer requestCustomer) {
-        Customer customer = customerRepository.findCustomerByCpfAndPhoneNumber(requestCustomer.getCpf(), requestCustomer.getPhoneNumber());
-        if (customer == null){
-            return new ResponseEntity<String>("Usuario não cadastrado", HttpStatus.BAD_REQUEST);
-        }
-
-        return new ResponseEntity<String>("Logado com sucesso", HttpStatus.OK);
+    public ResponseEntity<String> login(CustomerRequest customerRequest) {
+        return customerClient.customerRegister(customerRequest);
     }
 
-    public void hasRegister(Customer customer) throws RegisteredUserException {
-        Customer customers = customerRepository.findCustomerByCpfOrPhoneNumber(customer.getCpf(), customer.getPhoneNumber());
-        if (customers != null){
-            throw new RegisteredUserException("Usuario ja cadastrado");
-        }
-    }
 
 }
