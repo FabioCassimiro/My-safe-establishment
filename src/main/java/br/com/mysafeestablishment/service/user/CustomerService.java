afc7a5b6c8ebf582529f26f5e0e6de6b1c049765
Fabio Cassimiro <fabiocassimiro.project@gmail.com>
2021-08-23 19:32:54 -0300

    MSE-20210821 - ajuste de login

--- src/main/java/br/com/mysafeestablishment/service/user/CustomerService.java
+++ src/main/java/br/com/mysafeestablishment/service/user/CustomerService.java
@@ -2,10 +2,7 @@ package br.com.mysafeestablishment.service.user;
 
 import br.com.mysafeestablishment.api.client.CustomerClient;
 import br.com.mysafeestablishment.api.request.CustomerRequest;
-import br.com.mysafeestablishment.domain.user.Customer;
-import br.com.mysafeestablishment.exception.RegisteredUserException;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
