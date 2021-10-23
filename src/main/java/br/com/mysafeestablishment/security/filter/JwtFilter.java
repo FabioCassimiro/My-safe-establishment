c48f11292ee5257e324684297ea861d476483882
Fabio Cassimiro <fabiocassimiro.project@gmail.com>
2021-10-22 21:41:26 -0300

    MSE-20211022 - Ajuste de CORS

--- src/main/java/br/com/mysafeestablishment/security/filter/JwtFilter.java
+++ src/main/java/br/com/mysafeestablishment/security/filter/JwtFilter.java
@@ -48,6 +48,11 @@ public class JwtFilter extends OncePerRequestFilter {
                 SecurityContextHolder.getContext().setAuthentication(usernamePasswordAuthenticationToken);
             }
         }
+        httpServletResponse.setHeader("Access-Control-Allow-Origin", httpServletRequest.getHeader("Origin"));
+        httpServletResponse.setHeader("Access-Control-Allow-Credentials", "true");
+        httpServletResponse.setHeader("Access-Control-Allow-Methods", "POST, GET, OPTIONS, DELETE");
+        httpServletResponse.setHeader("Access-Control-Max-Age", "3600");
+        httpServletResponse.setHeader("Access-Control-Allow-Headers", "Content-Type, Accept, X-Requested-With, remember-me");
         filterChain.doFilter(httpServletRequest, httpServletResponse);
     }
 }
