67878fd5cfbcb9d534587a848d72d3c7e9fbddf7
Fabio Cassimiro <fabiocassimiro.project@gmail.com>
2021-11-01 15:39:08 -0300

    MSE-20211101 - Ajuste de tratamento de erro

---
+++ src/main/java/br/com/mysafeestablishment/api/client/CustomErrorDecoder.java
@@ -0,0 +1,25 @@
+package br.com.mysafeestablishment.api.client;
+
+import com.google.gson.Gson;
+import com.google.gson.JsonObject;
+import feign.Response;
+import feign.codec.ErrorDecoder;
+import org.apache.commons.io.Charsets;
+import org.apache.commons.io.IOUtils;
+
+import java.io.IOException;
+
+public class CustomErrorDecoder implements ErrorDecoder {
+    @Override
+    public Exception decode(String s, Response response) {
+        String body;
+        Gson gson = new Gson();
+        try {
+            body = IOUtils.toString(response.body().asInputStream(), Charsets.UTF_8);
+        } catch (IOException e) {
+            return e;
+        }
+        JsonObject json = gson.fromJson(body, JsonObject.class);
+        return new Exception(json.get("message").getAsString());
+    }
+}
